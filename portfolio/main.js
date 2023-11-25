08e4557813a98369c0b3309d177346437f6944a8
Donald <user@example.com>
2023-11-25 15:16:03 +0100

    adding css to home page

---
+++ portfolio/main.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const menuIcon = document.querySelector('.menu-icon');
+    const menuList = document.querySelector('.menu-list');
+  
+    menuIcon.addEventListener('click', function () {
+      menuList.classList.toggle('active');
+    });
+  });
+
+function myFunction(x) {
+    x.classList.toggle("change");
+}
